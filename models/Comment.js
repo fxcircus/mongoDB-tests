d18b791515eaf046a030c365b0cb05977c92aae6
Roy Daniel <roydanielr@gmail.com>
2022-05-12 06:56:10 -0400

    randId property for comment tests

--- models/Comment.js
+++ models/Comment.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const commentSchema = new Schema ({
     text: { type: String, required: true },
+    rendId: { type: Number, default: 1},
     post: { type: Schema.Types.ObjectId, ref: 'project' }
 },{
     timestamps: true
